1af6775a9951bfd3d126d98d3d1bf19b84910b54
simon <simon.baumgaertner@protonmail.com>
2021-10-21 12:40:13 +0200

    sorting algorithms

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] numbersToBeSorted = new int[] {1,24,12,312, 32, 12, 2, 32, 7, 12, 33 ,4,0};
-        System.out.println(Arrays.toString(numbersToBeSorted));
+        int[] arr = new int[] {1,24,12,312, 32, 12, 2, 32, 7, 12, 33 ,4,0};
+        System.out.println(Arrays.toString(arr));
 
-        for (int globalIndex = 1; globalIndex < numbersToBeSorted.length; globalIndex++) {
-            int value = numbersToBeSorted[globalIndex];
-            int workingIndex = globalIndex - 1;
-            while(workingIndex >= 0 && numbersToBeSorted[workingIndex] > value) {
-                numbersToBeSorted[workingIndex + 1] = numbersToBeSorted[workingIndex];
-                workingIndex = workingIndex - 1;
+        for (int i = 1; i < arr.length; i++) {
+            int value = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > value) {
+                arr[j + 1] = arr[j];
+                j--;
             }
-            numbersToBeSorted[workingIndex + 1] = value;
+            arr[j + 1] = value;
         }
-        System.out.println(Arrays.toString(numbersToBeSorted));
+        System.out.println(Arrays.toString(arr));
     }
 }
